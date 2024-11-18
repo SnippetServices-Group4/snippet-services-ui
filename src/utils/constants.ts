cc3e95d619541bd3678693ac540bff5096571fc6
Elias85974 <128708960+Elias85974@users.noreply.github.com>
2024-11-18 17:14:38 -0300

    Setting environment variables for cypress

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,6 @@
-export const FRONTEND_URL = process?.env?.FRONTEND_URL ?? "http://localhost:5173"
-export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:80"
-export const AUTH0_USERNAME = process?.env?.AUTH0_USERNAME ?? ""
-export const AUTH0_PASSWORD = process?.env?.AUTH0_PASSWORD ?? ""
-export const auth_audience: string = import.meta.env.VITE_AUTH0_AUDIENCE ?? "";
+// Hard code the values for the constants for cypress
+export const FRONTEND_URL = process.env.VITE_FRONTEND_URL as string ?? "http:/localhost:5173"
+export const BACKEND_URL = process?.env?.BACKEND_URL as string ?? "http:/localhost:80"
+export const AUTH0_USERNAME = process?.env?.AUTH0_USERNAME as string ?? ""
+export const AUTH0_PASSWORD = process?.env?.AUTH0_PASSWORD as string ?? ""
+export const auth_audience: string = process?.env?.VITE_AUTH0_AUDIENCE as string ?? ""
