89e0402b7ba4b6e4d8e0c266fc760a5b3d7d23e3
Elias85974 <128708960+Elias85974@users.noreply.github.com>
2024-11-20 15:49:09 -0300

    Using the constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,4 @@
-export const BACKEND_URL: string = import.meta.env.VITE_BACKEND_URL as string ?? "http:/localhost:80"
-export const auth_audience: string = import.meta.env.VITE_AUTH0_AUDIENCE as string ?? ""
+export const BACKEND_URL: string = import.meta.env.VITE_BACKEND_URL as string ?? "http:/localhost:80";
+export const auth_audience: string = import.meta.env.VITE_AUTH0_AUDIENCE as string ?? "";
+export const AUTH0_DOMAIN: string = import.meta.env.VITE_AUTH0_DOMAIN as string ?? "";
+export const AUTH0_CLIENT_ID: string = import.meta.env.VITE_AUTH0_CLIENT_ID as string ?? "";
